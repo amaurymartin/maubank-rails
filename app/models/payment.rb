0619ecf57f2ef423aa0ff16e89106c5f56fb6634
Amaury Martin <amaury.conde@hotmail.com>
2021-12-10 03:31:48 -0300

    feat(payment): add payment model validations

---
+++ app/models/payment.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Payment < ApplicationRecord
+  include Keyable
+
+  belongs_to :category, optional: true
+  belongs_to :wallet
+
+  validates :key, presence: true, uniqueness: true
+  validates :effective_date, presence: true
+  validates :amount, numericality: {
+    greater_than: -1_000_000_000.00,
+    other_than: 0.00,
+    less_than: 1_000_000_000.00
+  }
+
+  delegate :user, to: :wallet
+end
