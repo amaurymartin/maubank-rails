afb1d7e9c392f7e63872c529028f5c046825e145
Amaury Martin <amaury.conde@hotmail.com>
2021-12-14 02:23:22 -0300

    feat(controllers): add record not found handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    head :not_found
+  end
 end
