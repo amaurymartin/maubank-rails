88d7e6d7afb50f5b860409bc72e4aa7401056a80
Amaury Martin <amaury.conde@hotmail.com>
2022-01-15 18:02:37 -0300

    refactor(goal): make description a sequence

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :goal do
     user
-    description { 'First goal' }
+    sequence(:description) { |n| "Goal #{n}" }
     amount { Faker::Number.between(from: 0.01, to: 999_999_999.99) }
     starts_at { Time.zone.today.beginning_of_year }
     ends_at { Time.zone.today.end_of_year }
