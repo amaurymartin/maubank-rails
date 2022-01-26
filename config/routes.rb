1ada74597f24c60b0a065b1ef329f86be3cd0fc3
Amaury Martin <amaury.conde@hotmail.com>
2022-01-25 23:37:53 -0300

    feat(payment): add payments routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
 
     resources :sessions, only: :create
     resources :users, param: :key, except: :index
-    resources :wallets, param: :key, except: :show
+    resources :wallets, param: :key, except: :show do
+      resources :payments, only: %i[create index], module: :wallets
+    end
     resources :goals, param: :key
     resources :categories, param: :key, shallow: true do
       resources :budgets, param: :key, except: :index
     end
+    resources :payments, param: :key, except: :create
   end
 end
