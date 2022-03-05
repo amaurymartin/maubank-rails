11cfd9bd10550f7fb8fb01834263e275f29e8574
Amaury Martin <amaury.conde@hotmail.com>
2022-03-05 01:45:33 -0300

    refactor(goal): improve goal model, specs, and factory

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -12,12 +12,11 @@ class Goal < ApplicationRecord
             numericality: { greater_than: 0.00, less_than: 1_000_000_000.00 }
   validates :starts_at, presence: true
   validates :ends_at, presence: true
-
-  validate :ends_at_cannot_be_before_starts_at
+  validate :ends_at_must_be_after_starts_at
 
   private
 
-  def ends_at_cannot_be_before_starts_at
+  def ends_at_must_be_after_starts_at
     return if starts_at.nil? || ends_at.nil? || ends_at > starts_at
 
     errors.add(:ends_at, :invalid)
--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     user
     sequence(:description) { |n| "Goal #{n}" }
     amount { Faker::Number.between(from: 0.01, to: 999_999_999.99) }
-    starts_at { Time.zone.today.beginning_of_year }
-    ends_at { Time.zone.today.end_of_year }
+    starts_at { Date.current.beginning_of_year }
+    ends_at { Date.current.end_of_year }
   end
 end
