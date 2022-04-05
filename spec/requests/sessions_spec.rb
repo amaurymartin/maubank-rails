0e96935207071603c5b48004183f6418904b59d6
Amaury Martin <amaury.conde@hotmail.com>
2022-04-05 01:16:57 -0300

    test(session): improve sessions specs

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -4,57 +4,67 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
   let(:response_body) { JSON.parse(response.body).deep_symbolize_keys }
+  let(:current_user) { create(:user) }
+  let(:session_keys) { %i[access_token expires_at] }
 
   describe 'POST /sessions' do
     def make_request
-      post sessions_path, params: { session: session_create_params }
+      post(sessions_path, params: { session: session_params })
     end
 
-    let(:session_create_json_keys) { %i[access_token expires_at] }
-    let(:user) { create(:user) }
-    let(:session_create_params) do
-      { email: user.email, password: user.password }
+    let(:session_params) do
+      { email: current_user.email, password: current_user.password }
     end
 
     context 'with success' do
       it :aggregate_failures do
         expect { make_request }.to change(AccessToken, :count).by(1)
         expect(response).to have_http_status(:created)
-        expect(response_body[:session].keys)
-          .to match_array(session_create_json_keys)
+        expect(response_body[:session].keys).to match_array(session_keys)
       end
     end
 
     context 'with invalid email' do
-      let(:session_create_params) do
-        { email: 'invalid_email', password: user.password }
+      let(:session_params) do
+        { email: 'invalid_email', password: current_user.password }
       end
 
       it :aggregate_failures do
         expect { make_request }.not_to change(AccessToken, :count)
         expect(response).to have_http_status(:unauthorized)
-        expect(response_body[:errors]).to be_present
+        expect(response_body[:errors]).not_to be_empty
       end
     end
 
-    context 'with invalid password' do
-      let(:session_create_params) do
-        { email: user.email, password: 'invalid_password' }
+    context 'with valid but wrong email' do
+      let(:session_params) do
+        { email: 'wrong@email.com', password: current_user.password }
       end
 
       it :aggregate_failures do
         expect { make_request }.not_to change(AccessToken, :count)
         expect(response).to have_http_status(:unauthorized)
-        expect(response_body[:errors]).to be_present
+        expect(response_body[:errors]).not_to be_empty
+      end
+    end
+
+    context 'with wrong password' do
+      let(:session_params) do
+        { email: current_user.email, password: 'wrong_password' }
+      end
+
+      it :aggregate_failures do
+        expect { make_request }.not_to change(AccessToken, :count)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response_body[:errors]).not_to be_empty
       end
     end
 
     context 'with unexpected error' do
-      let(:access_token) { build(:access_token, user: nil) }
-
       before do
-        allow(AccessToken).to receive(:new).and_return(access_token)
-        allow(access_token).to receive(:save).and_return(false)
+        # rubocop:disable RSpec/AnyInstance
+        allow_any_instance_of(AccessToken).to receive(:save).and_return(false)
+        # rubocop:enable RSpec/AnyInstance
       end
 
       it :aggregate_failures do
