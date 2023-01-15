6cafe6658d836b1a03b17d7b0ff1157e143f04bd
Amaury Martin <amaury.conde@hotmail.com>
2023-01-15 05:19:00 -0500

    docs: annotate files

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: goals
+#
+#  id          :bigint           not null, primary key
+#  amount      :decimal(11, 2)   not null
+#  description :text             not null
+#  ends_at     :date             not null
+#  key         :uuid             not null
+#  starts_at   :date             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_goals_on_key                      (key) UNIQUE
+#  index_goals_on_user_id                  (user_id)
+#  index_goals_on_user_id_and_description  (user_id,description) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Goal < ApplicationRecord
   include Keyable
 
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: access_tokens
+#
+#  id         :bigint           not null, primary key
+#  revoked_at :datetime
+#  token      :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_access_tokens_on_token    (token) UNIQUE
+#  index_access_tokens_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe AccessToken do
